4a579690c98362103e51eb7a6d0683f0d69f1fc5
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-02-23 10:16:30 -0500

    proy-pagos

---
+++ src/main/java/pe/idat/edu/pagos_cloud_client/PagosCloudClientApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.pagos_cloud_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosCloudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/edu/pagos_cloud_client/PagosCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.pagos_cloud_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosCloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
